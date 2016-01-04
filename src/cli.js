e6c4b596dbe3cc65dd7cec7f4cb1fd27553d4a1b
Julien Fontanet <julien.fontanet@isonoe.net>
2016-01-04 18:43:39 +0100

    Initial commit.

---
+++ src/cli.js
@@ -0,0 +1,164 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk'
+import eventToPromise from 'event-to-promise'
+import execPromise from 'exec-promise'
+import minimist from 'minimist'
+import { create as createHttpServer } from 'http-server-plus'
+import { load as loadConfig } from 'app-conf'
+import { readFile } from 'fs-promise'
+
+import {
+  name as pkgName,
+  version as pkgVersion
+} from '../package'
+
+import {
+  createReadableCopies,
+  proxyHttpsRequest,
+  splitHost
+} from './utils'
+import {
+  isXmlRpcRequest,
+  parseRequest,
+  parseResponse
+} from './xml-rpc'
+
+// ===================================================================
+
+const paintArg = chalk.yellow
+
+const requiredArg = name => {
+  const message = `Missing argument: <${paintArg(name)}>`
+
+  throw message
+}
+
+// ===================================================================
+
+const wrapAsync = asyncFn => async function () {
+  try {
+    return await asyncFn.apply(this, arguments)
+  } catch (error) {
+    console.error(error.stack || error)
+
+    throw error
+  }
+}
+
+// -------------------------------------------------------------------
+
+const COMMANDS = {
+  async proxy (args) {
+    let {
+      bind = '0',
+      _: [
+        remote = requiredArg('remote address')
+      ]
+    } = minimist(args, {
+      string: 'bind'
+    })
+
+    bind = {
+      certificate: await readFile(`${__dirname}/../cert.pem`),
+      key: await readFile(`${__dirname}/../key.pem`),
+      ...splitHost(bind)
+    }
+    remote = {
+      protocol: 'https:',
+      ...splitHost(remote)
+    }
+
+    // ---------------------------------
+
+    const handleRequest = async (req, res) => {
+      console.log('[%s] - Not XML-RPC', chalk.blue(req.url))
+
+      await proxyHttpsRequest(remote, req)
+        .pipe(res)
+    }
+
+    const handleXmlRpcRequest = async (req, res) => {
+      const [ req1, req2 ] = createReadableCopies(2, req)
+      const [ res1 ] = createReadableCopies(1, await proxyHttpsRequest({
+        ...req,
+        ...remote
+      }, req1))
+      res1.pipe(res)
+
+      const { method, params } = await parseRequest(req2)
+      console.log(
+        '[%s] %s(%s)',
+        chalk.blue(req.url),
+        chalk.bold.red(method),
+        params
+      )
+    }
+
+    // ---------------------------------
+
+    const server = createHttpServer(wrapAsync(
+      (req, res) => isXmlRpcRequest(req)
+        ? handleXmlRpcRequest(req, res)
+        : handleRequest(req, res)
+    ))
+
+    console.log(await server.listen(bind))
+
+    await eventToPromise(server, 'close')
+  }
+}
+
+// ===================================================================
+
+const usage = `Usage: ${pkgName} proxy [--bind <local address>] <remote address>
+
+  Create a XML-RPC proxy which forward requests from <local address>
+  to <remote address>.
+
+  <local address>:  [<hostname>]:<port>
+  <remote address>: <hostname>[:<port = 443>]
+
+${pkgName} v${pkgVersion}
+`.replace(/<([^>]+)>/g, (_, arg) => `<${paintArg(arg)}>`)
+
+execPromise(async args => {
+  const {
+    help = false,
+    _: restArgs,
+    '--': restRestArgs
+  } = minimist(args, {
+    boolean: 'help',
+    alias: {
+      help: 'h'
+    },
+    stopEarly: true,
+    '--': true
+  })
+
+  if (help) {
+    return usage
+  }
+
+  // Work around https://github.com/substack/minimist/issues/71
+  restArgs.push('--')
+  ;[].push.apply(restArgs, restRestArgs)
+
+  const [ commandName, ...commandArgs ] = restArgs
+
+  if (commandName === '--') {
+    throw new Error('missing <command>')
+  }
+
+  const command = COMMANDS[commandName]
+  if (!command) {
+    throw new Error(`invalid <command>: ${commandName}`)
+  }
+
+  return command.call(
+    {
+      config: await loadConfig('xapi-inspector')
+    },
+    commandArgs
+  )
+})
